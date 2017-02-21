057ccdce062f1a72bc96657824ef4c4183bc42e0
areeyatipyasothi <areeyatipyasothi@10.0.0.75>
2017-02-21 00:55:49 -0800

    A comment!

---
+++ src/newspaper/distribution/NewspaperDistribution.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newspaper.distribution;
+
+/**
+ *
+ * @author areeyatipyasothi
+ */
+public class NewspaperDistribution {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //boop boop be doop
+    }
+    
+}
