ffaf4ac5d7e756dbd2ca8efec52897b3b54281df
areeyatipyasothi <areeyatipyasothi@10.0.0.75>
2017-02-22 00:13:44 -0800

    God what is this (added a scanner object)

--- src/newspaper/distribution/input.java
+++ src/newspaper/distribution/input.java
@@ -4,11 +4,13 @@
  * and open the template in the editor.
  */
 package newspaper.distribution;
-
+import java.util.Scanner;
 /**
  *
  * @author areeyatipyasothi
  */
 public class input {
+    Scanner s = new Scanner(System.in);
+    
     
 }
